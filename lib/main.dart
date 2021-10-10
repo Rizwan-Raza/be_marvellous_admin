36d52ec6bf5abc79e6273267f3bc4ddc30419cdb
Rizwan Raza <rizwan.raza987@gmail.com>
2021-10-11 04:07:30 +0530

    Title updated

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Be Marvellous | Admin Panel',
         theme: ThemeData(
           fontFamily: 'Product-Sans',
           primarySwatch: Colors.red,
